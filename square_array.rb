e91a60a723a65fc7c9ef1a817b9cd6076565ce65
j.d.ames64@gmail.com <j.d.ames64@gmail.com>
2018-06-17 04:25:35 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
   new_array = []
     array.each do |square|
-        new_array << square ** 2
+        new_square = square ** 2
+        new_square << square
     end
     return new_array
 end
